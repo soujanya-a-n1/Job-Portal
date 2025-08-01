d6f7b761c70464a7966cbf78934d644727b9bc49
soujanya-a-n1 <soujanyanavi1@gmail.com>
2025-08-01 15:29:02 +0530

    job application

---
+++ backend/controller/jobapplicationcontroller.js
@@ -0,0 +1,99 @@
+import mongoose from "mongoose";
+import JobApplication from "../models/jobapplication.js";
+import jobmodel from "../models/jobsmodel.js";
+import usermodel from "../models/usermodel.js";
+import jobsapplicationmodel from "../models/jobapplication.js";
+import jobrouter from '../router/jobsroutes.js';
+import jobapplictionrouter from '../router/jobapplicationroutes.js';
+
+
+export const applyjob=async(req,res)=>{
+    try {
+        const jobid=req.params.jobid;
+        const userid=req.params.userid;
+
+        if(!jobid || !userid){
+            return res.status(400).json({error:"jobid or userid missing in params"})
+        }
+
+        //check with that userid user exist or not ,if not exist userid not  valid
+        const user=await usermodel.findById(userid);
+        if(!user){
+            return res.status(404).json({error:"userid in valid"})
+        }
+
+        //check with that jobid job exist or not ,if not exist jobid not  valid
+        const job=await jobmodel.findById(jobid);
+        if(!job){
+            return res.status(404).json({error:"jobid in valid"})
+        }
+
+        const newappliction=new jobsapplicationmodel({...req.body,jobid:jobid,userid:userid});
+        await newappliction.save();
+        return res.status(200).json({message:"job applied successfully",application:newappliction});
+    } catch (error) {
+        return res.status(500).json({error:'internal server error'+error.message});
+    }
+}
+
+export const getapplicationsoflogineduser=async(req,res)=>{
+    try {
+        let userid=req.params.userid;
+        if(!userid){
+            return res.status(400).json({error:"userid missing in params"})
+        }
+        //check usedr exist db or not
+        let user=await usermodel.findById(userid);
+        if(!user){
+            return res.status(404).json({error:"user not found"})
+        }
+
+        let applications=await jobsapplicationmodel.find({userid:userid}).populate('jobid').populate('userid');
+        return res.status(200).json({message:"applications fetched successfully",applications:applications})
+    } catch (error) {
+        return res.status(500).json({error:'internal server error'+error.message});
+    }
+}
+
+export const updateapplication=async(req,res)=>{
+    try {
+        const id=req.params.id;
+        if(!id){
+            return res.status(400).json({error:"id is required"});
+        }
+        const updatedApplication=await jobsapplicationmodel.findByIdAndUpdate(id, req.body);
+        return res.status(200).json({message:"application updated successfully"});
+    } catch (error) {
+        return res.status(500).json({error:'internal server error'+error.message});
+    }
+}
+
+export const deleteapplication=async(req,res)=>{
+    try{
+        const id=req.params.id;
+        if(!id){
+            return res.status(400).json({error:'id is required'});
+        }
+        const deleteapplication=await jobsapplicationmodel.findByIdAndDelete(id,res.bosy);
+        return res.status(200).json({message:"application deleted successfully"});
+    }catch(error){
+        return res.status(500).json({error:"internal server error"+error.message});
+    }
+}
+
+export const getapplicationofparticularjob=async(req,res)=>{
+    try {
+        let jobid=req.params.jobid;
+        if(!jobid){
+            return res.status(400).json({error:"jobid missing in params"})
+        }
+        let job=await jobmodel.findById(jobid)
+        if(!job){
+            return res.status(404).json({error:"job not found"})
+        }
+        const applications=await jobsapplicationmodel.find({jobid:jobid}).populate('jobid').populate('userid');
+        return res.status(200).json({message:"applications fetched successfully",applications:applications})
+    } catch (error) {
+        return res.status(500).json({error:'internal server error'+error.message});
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectdb from "./db/dbconnect.js";
-import userrouter from './router/userrouter.js';
-import jobrouter from './router/jobsrouter.js';
+import userrouter from './router/userroutes.js';
+import jobrouter from './router/jobsroutes.js';
+import jobapplictionrouter from './router/jobapplicationroutes.js'
 import cors from 'cors'
-
 const app = express();
 dotenv.config();//load env variables
 connectdb(); 
 //MIDDLEWARES
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors());
 app.get('/',(req,res)=>{
     res.status(200).send("server is running fine");
 })
@@ -21,7 +21,7 @@ app.get('/api/home',(req,res)=>{
 //ROUTES
 app.use('/api',userrouter)
 app.use('/api',jobrouter)
-app.use(cors());
+app.use('/api',jobapplictionrouter)
 let port=process.env.port || 5051;
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
